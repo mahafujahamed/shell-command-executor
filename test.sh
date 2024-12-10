7fd1938db2dcce530342e0365003825abb73e016
mahafuj ahamed <mahafujahamed068@gmail.com>
2024-12-10 22:27:01 +0600

    testing file added

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Mahafuj Ahamed"
+echo "testing shell command"
---
+++ test1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -n "Enter your name:"
+read name
+echo -n "Enter your number:"
+read number
+echo "Your Name: $name"
+echo "Your Number: $number"
+echo "Second Level Testing"
+
